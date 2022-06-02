731fc5996d715e8b2926939249cf8be9690efb34
Kd <umarkhurshid3@gmail.com>
2022-06-03 04:13:28 +0530

    new update

--- src/Components/HeaderHome/index.js
+++ src/Components/HeaderHome/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Video from './media/bgvideo2(1).mp4';
-
+import { isMobile } from 'react-device-detect';
 import {
   HeroContainer,
   HeroP,
@@ -15,11 +15,7 @@ import {
 import styled from 'styled-components';
 
 const Gradients = styled.div`
-  background-image: linear-gradient( 
-    180deg,
-    rgba(0, 0, 0, 0.49) 20%,
-    rgba(0, 64, 255, 0.23) 100%
-  );
+
   width: 100%;
   height: 100%;
   transform: rotate(-180deg);
