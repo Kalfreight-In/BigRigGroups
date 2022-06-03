5e15a0f05cc40b5374885ce632f6cff244a2263c
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-04 04:02:37 +0530

    changes in footer

--- src/Components/Footernewcomp/Footer.js
+++ src/Components/Footernewcomp/Footer.js
@@ -172,8 +172,8 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="flex flex-auto flex-row  mb-8 lg:mb-2 mt-16 mr-12">
-            <div className="lg:flex-1 flex-1 flex justify-end lg:w-1/6 ">
+          <div className="flex flex-auto flex-row  mb-8 lg:mb-2 lg:mt-8 ">
+            <div className="lg:flex-1 flex-1 flex lg:justify-end lg:w-1/6 ml-8">
               <div>
                 <div className="text-white">
                   <h1 className=" font-bold  2xl:text-footerheading text-desc">
