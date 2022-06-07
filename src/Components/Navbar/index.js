441f9c89342b109106571e37619dc99f5b4d31b2
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-08 04:31:17 +0530

    changes

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 import { animateScroll as scroll, Link } from 'react-scroll';
-// import ErrorBoundary from '../ErrorBoundary'; 
+// import ErrorBoundary from '../ErrorBoundary';
 import { useNavigate } from 'react-router-dom';
 import {
   Nav,
@@ -33,7 +33,7 @@ const Navbar = ({ toggle }) => {
   // }
   // const initialState = loadLogo(window.location.pathname);
   const [logo, setlogo] = useState(
-    "https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/navlogo.svg"
+    'https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/navlogo.svg'
   );
   const [scrollNav, setScrollNav] = useState(false);
   const changeNav = () => {
@@ -55,28 +55,24 @@ const Navbar = ({ toggle }) => {
       );
       console.log(`${window.location.pathname} and then there is ${logo}`);
       setScrollNav(true);
-    }
-    if (window.location.pathname == '/BigRigGroups') {
+    } else if (window.location.pathname == '/BigRigGroups') {
       setlogo(
         'https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/navlogo.svg'
       );
       console.log(`${window.location.pathname} and then there is ${logo}`);
-    }
-    if (window.location.pathname == '/partz') {
+    } else if (window.location.pathname == '/partz') {
       setlogo(
         'https://raw.githubusercontent.com/Kalfreight-In/BigRigGroups/main/src/assets/Images/icons/Parts-nav-logo.png'
       );
       console.log(`${window.location.pathname} and then there is ${logo}`);
-    }
-    if (window.location.pathname == '/tires') {
-      setlogo(    
+    } else if (window.location.pathname == '/tires') {
+      setlogo(
         'https://raw.githubusercontent.com/Kalfreight-In/BigRigGroups/main/src/assets/Images/icons/tires-nav-logo.png'
       );
       console.log(`${window.location.pathname} and then there is ${logo}`);
-    }
-    else{
+    } else {
       setlogo(
-        "https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/navlogo.svg"
+        'https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/navlogo.svg'
       );
     }
   }, [window.location.pathname]);
@@ -92,13 +88,13 @@ const Navbar = ({ toggle }) => {
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#fff", size: "1em" }}>
+      <IconContext.Provider value={{ color: '#fff', size: '1em' }}>
         <Nav scrollNav={scrollNav}>
-          <div className="flex flex-row ml-8">
-            <NavLogo to="/BigRigGroups" onClick={toggleHome}>
+          <div className="flex flex-row ml-8 xl:mx">
+            <NavLogo to="/BigRigGroups" onClick={toggleHome} className="w-max">
               <img
                 // className="2xl:w-56 w-44  2xl:h-44 h-32  "
-                className="pt-6"
+                className="pt-6 "
                 src={logo}
                 alt="logo"
                 width=" 70%"
@@ -110,7 +106,7 @@ const Navbar = ({ toggle }) => {
                 <NavLinks
                   to="/BigRigGroups"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall  max-w-screen-lg:text-xs md:text-xs"
+                  className="2xl:text-16px text-navsmall  max-w-screen-lg:text-xs md:text-xs "
                 >
                   HOME
                 </NavLinks>
@@ -120,13 +116,13 @@ const Navbar = ({ toggle }) => {
                 <NavLinks
                   to="/trailerandleasing"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs"
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs w-max"
                 >
                   TRAILER & LEASING
                 </NavLinks>
               </NavItem>
 
-              <NavItem>
+              {/* <NavItem>
                 <NavLinks
                   to="/partz"
                   onClick={toggleHome}
@@ -134,14 +130,14 @@ const Navbar = ({ toggle }) => {
                 >
                   PARTZ
                 </NavLinks>
-              </NavItem> 
+              </NavItem> */}
 
               <NavItem>
                 <a href="/BigRigGroups/tires">
                   <NavLinks
                     to="/tires"
                     onClick={toggleHome}
-                    className="2xl:text-16px text-navsmall lg:text-xs md:text-xs"
+                    className="2xl:text-16px text-navsmall lg:text-xs md:text-xs w-max"
                   >
                     TIRES & SERVICES
                   </NavLinks>
@@ -150,9 +146,19 @@ const Navbar = ({ toggle }) => {
 
               <NavItem>
                 <NavLinks
+                  to="/partz"
+                  onClick={toggleHome}
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs "
+                >
+                  PARTZ
+                </NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks
                   to="ContactSection"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs"
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs "
                 >
                   <Link
                     to="ContactSection"
@@ -171,17 +177,17 @@ const Navbar = ({ toggle }) => {
                 <NavLinks
                   to="/Contact"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs" 
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs w-max"
                 >
                   NEWS & EVENTS
                 </NavLinks>
-              </NavItem> 
+              </NavItem>
 
               <NavItem>
                 <NavLinks
                   to="/Contact"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs"
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs "
                 >
                   LOCATIONS
                 </NavLinks>
@@ -190,7 +196,7 @@ const Navbar = ({ toggle }) => {
                 <NavLinks
                   to="/Contact"
                   onClick={toggleHome}
-                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs"
+                  className="2xl:text-16px text-navsmall lg:text-xs md:text-xs "
                 >
                   CREDIT APPLICATION
                 </NavLinks>
