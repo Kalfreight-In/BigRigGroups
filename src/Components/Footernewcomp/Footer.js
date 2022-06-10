e6783e5b9fb6752926123ccdee47eefb67703953
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-06-11 05:26:00 +0530

    make semi final buold

--- src/Components/Footernewcomp/Footer.js
+++ src/Components/Footernewcomp/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
 import { AiOutlineClockCircle } from 'react-icons/ai';
-import {RiFacebookCircleLine} from 'react-icons/ri';
+import { RiFacebookCircleLine } from 'react-icons/ri';
 // import {SiInstagram} from 'react-icons/si';
-import {AiOutlineInstagram} from 'react-icons/ai';
-import {TiSocialLinkedinCircular} from 'react-icons/ti'; 
-import { animateScroll as scroll, Link } from "react-scroll";
-
-
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { TiSocialLinkedinCircular } from 'react-icons/ti';
+import { animateScroll as scroll, Link } from 'react-scroll';
 
 export default function Footer() {
+  {
+    window.location.pathname == '/'
+      ? console.log(`${window.location.pathname} yesssssssssssssssssss`)
+      : console.log(`${window.location.pathname} nooooooooooooooo`);
+  }
   return (
     <>
       <div className="bg-Lightblue">
@@ -17,15 +20,15 @@ export default function Footer() {
           className="flex lg:flex-row flex-col 2xl:pb-8 xl:pt-8 lg:pt-4 lg:flex lg:justify-between"
           style={{
             backgroundImage:
-              "url(" +
-              "https://raw.githubusercontent.com/Kalfreight-In/BigRigGroups/main/src/assets/Map/map-bg.png" +
-              ")",
+              'url(' +
+              'https://raw.githubusercontent.com/Kalfreight-In/BigRigGroups/main/src/assets/Map/map-bg.png' +
+              ')',
 
-            backgroundPosition: "center",
-            backgroundSize: "cover",
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
 
-            backgroundRepeat: "no-repeat",
-            width: "full",
+            backgroundRepeat: 'no-repeat',
+            width: 'full',
           }}
         >
           <div
@@ -144,18 +147,31 @@ export default function Footer() {
                   >
                     About us
                   </a> */}
-
-                  <Link
-                    className="block md:text-16px text-navsmall hover:text-yellow-shadowhover cursor-pointer"
-                    to="aboutus"
-                    smooth={true}
-                    duration={1000}
-                    spy={true}
-                    exact={true}
-                    offset={-80}
-                  >
-                    About us
-                  </Link>
+                  {window.location.pathname == '/' ? (
+                    <Link
+                      className="block md:text-16px text-navsmall hover:text-yellow-shadowhover cursor-pointer"
+                      to="aboutus"
+                      smooth={true}
+                      duration={1000}
+                      spy={true}
+                      exact={true}
+                      offset={-80}
+                    >
+                      About us
+                    </Link>
+                  ) : (
+                    <a
+                      className="block md:text-16px text-navsmall hover:text-yellow-shadowhover cursor-pointer"
+                      href="/#aboutus"
+                      smooth={true}
+                      duration={1000}
+                      spy={true}
+                      exact={true}
+                      offset={-80}
+                    >
+                      About us
+                    </a>
+                  )}
                 </div>
                 <div>
                   <a
@@ -212,7 +228,7 @@ export default function Footer() {
                       href=""
                       className="block md:text-16px text-navsmall hover:text-yellow-shadowhover"
                     >
-                      hr@bigrigcanada.com{" "}
+                      hr@bigrigcanada.com{' '}
                     </a>
                   </div>
                   <div className="flex flex-row space-x-2">
@@ -229,15 +245,15 @@ export default function Footer() {
                   </div>
                   <div className="flex flex-row space-x-2">
                     <div className="block md:text-16px text-navsmall hover:text-yellow-shadowhover">
-                      {" "}
-                      Partz{" "}
+                      {' '}
+                      Partz{' '}
                     </div>
                     <span> » </span>
                     <a
                       href=""
                       className="block md:text-16px text-navsmall hover:text-yellow-shadowhover"
                     >
-                      partz@bigrigcanada.com{" "}
+                      partz@bigrigcanada.com{' '}
                     </a>
                   </div>
 
@@ -282,8 +298,8 @@ export default function Footer() {
                       <RiFacebookCircleLine
                         onClick={() =>
                           window.open(
-                            "https://www.facebook.com/BigRiggroups/",
-                            "_blank"
+                            'https://www.facebook.com/BigRiggroups/',
+                            '_blank'
                           )
                         }
                         className="text-white  h-1.8rem w-1.8rem mx-4 cursor-pointer hover:drop-shadow-socialicons hover:text-facebook ease-in duration-300"
@@ -291,8 +307,8 @@ export default function Footer() {
                       <AiOutlineInstagram
                         onClick={() =>
                           window.open(
-                            "https://www.instagram.com/bigriggroup/",
-                            "_blank"
+                            'https://www.instagram.com/bigriggroup/',
+                            '_blank'
                           )
                         }
                         className="text-white h-1.8rem w-1.8rem mx-4 cursor-pointer hover:drop-shadow-socialicons hover:text-instagram ease-in duration-300"
@@ -300,8 +316,8 @@ export default function Footer() {
                       <TiSocialLinkedinCircular
                         onClick={() =>
                           window.open(
-                            "https://www.linkedin.com/company/bigriggroup",
-                            "_blank"
+                            'https://www.linkedin.com/company/bigriggroup',
+                            '_blank'
                           )
                         }
                         className="text-white   h-8 w-8 mx-4  cursor-pointer hover:drop-shadow-socialicons hover:text-linkden ease-in duration-300 "
