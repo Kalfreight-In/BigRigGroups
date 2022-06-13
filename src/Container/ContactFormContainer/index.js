bff8446fc006040667c4f8d235d6afe619d69d98
Kd <umarkhurshid3@gmail.com>
2022-06-14 01:53:55 +0530

    contactform in mobile device

--- src/Container/ContactFormContainer/index.js
+++ src/Container/ContactFormContainer/index.js
@@ -4,7 +4,10 @@ import Contactform from '../../Components/ContactForm/';
 export const ContactFormContainer = () => {
   return (
     // <div id="maincontainer" className='w-70vh' >
-    <div className=" flex md:flex lg:w-screen w-fit    lg:flex-row flex-col align-center justify-center " id='ContactSection'> 
+    <div
+      className=" flex md:flex lg:w-screen w-fit    lg:flex-row flex-col-reverse align-center justify-center "
+      id="ContactSection"
+    >
       <div className="lg:mt-16 lg:mb-16 h-fit flex content-center justify-center  flex-1 ">
         {/* <div>
           <h1 className="xl:text-5xl text-Heading font-semibold md:text-2xl link link-underline link-underline-black pb-8">
@@ -13,7 +16,7 @@ export const ContactFormContainer = () => {
           <p className="mt-2 text-Description font-desc text-desc ">
             we are ready to work on a project f any complexity
           </p>
-        </div> */} 
+        </div> */}
 
         {/* <Formcomp /> */}
         <Contactform></Contactform>
@@ -27,11 +30,16 @@ export const ContactFormContainer = () => {
       </div> */}
       <div className="flex-1 ">
         <div
-          className={'md:h-screen h-mobileMainImageHeight w-full bg-cover bg-no-repeat bg-center '}   
+          className={
+            "md:h-screen h-mobileMainImageHeight w-full bg-cover bg-no-repeat bg-center "
+          }
           style={{
-            backgroundImage: `url(https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/redtruckConTact.png)`, 
+            backgroundImage: `url(https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/master/src/assets/Images/redtruckConTact.png)`,
           }}
         >
+          <h1 className="text-Heading text-3xl font-bold ml-4 flex justify-center items-center">
+            Contact Us
+          </h1>
           {/* <img
           className="w-full"
           src="https://raw.githubusercontent.com/kalfreight-in/BigRigGroups/e6c3841952235a1866804c85e994f1d5c540c214/src/assets/Images/rednewtruck.svg"
