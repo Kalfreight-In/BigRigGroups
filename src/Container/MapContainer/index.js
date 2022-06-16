35f385c781da5612b8ea6c5391136f3f42a75f7e
Kd <umarkhurshid3@gmail.com>
2022-06-17 03:55:46 +0530

    mapcontainer see more link update

--- src/Container/MapContainer/index.js
+++ src/Container/MapContainer/index.js
@@ -6,28 +6,19 @@ import {Link} from "react-router-dom";
 export const MapConatiner = () => {
   const [AfhoverRef, AfisHovered] = useHover();
   const [CahoverRef, CaisHovered] = useHover();
-  const [TorantoRef, TorantoisHovered] = useHover();
-  const [SureyRef, SureyisHovered] = useHover();
-  const [EdmentonRef, EdmentonisHovered] = useHover();
   return (
     <div
       id="Location"
       className="bg-Lightblue flex flex-col-reverse  md:flex-row p-8 "
     >
       <div className=" svg-container">
-        <Map
-          Af={AfisHovered}
-          Ca={CaisHovered}
-          toranto={TorantoisHovered}
-          surey={SureyisHovered}
-          edmenton={EdmentonisHovered}
-        ></Map>
+        <Map Af={AfisHovered} Ca={CaisHovered}></Map>
       </div>
 
       <div className=" md:6/12 flex-col flex content-center  flex-1 md:ml-8 ml-0 ">
         <div className=" flex-col flex content-center justify-center md:w-4/3 flex-1 self-start items-start m-4 2xl:ml-12 xl:ml-8 mt-2 ">
           <div>
-            <h3 className="xl:text-4xl md:self-start text-white font-bold md:text-3xl text-3xl md:w-max text-center">
+            <h3 className="xl:text-4xl md:self-start text-white font-bold md:text-2xl text-3xl md:w-max text-center">
               National Footprint:
             </h3>
             <div className="mt-2 text-white font-desc text-descnew text-center">
@@ -36,11 +27,11 @@ export const MapConatiner = () => {
               <span>For assistance in the USA. </span>
 
               <Link
-                href="/mappage"
+                to="/mappage"
                 className="hover:text-yellow-shadowhover transition  text-yellow-shadowhover"
               >
                 click here
-              </Link>
+              </Link>  
             </div>
           </div>
           <div className="flex flex-row items-center content-start mt-2 md:ml-0 ">
@@ -55,7 +46,7 @@ export const MapConatiner = () => {
             ></motion.img>
             <div>
               <h4
-                className="mt-4 md:text-xl  text-lg text-white md:font-bold font-extrabold cursor-pointer"
+                className="mt-4 md:text-sm  text-lg text-white md:font-bold font-extrabold md:text-subheading cursor-pointer"
                 ref={CahoverRef}
               >
                 Abbotsford
@@ -77,7 +68,7 @@ export const MapConatiner = () => {
             ></motion.img>
             <div>
               <h4
-                className="mt-4 md:text-xl text-lg  text-white md:font-bold font-extrabold  cursor-pointer"
+                className="mt-4 md:text-sm text-lg  text-white md:font-bold font-extrabold md:text-subheading cursor-pointer"
                 ref={AfhoverRef}
               >
                 Calgary
@@ -102,13 +93,8 @@ export const MapConatiner = () => {
               width="60"
               className="pr-8 "
             ></motion.img>
-            <div className="cursor-pointer">
-              <div
-                className=" text-white font-desc text-descnew"
-                ref={TorantoRef}
-              >
-                Toronto
-              </div>
+            <div>
+              <div className=" text-white font-desc text-descnew">Toronto</div>
             </div>
           </div>
           <div className="flex flex-row items-center content-start mt-2 ">
@@ -121,13 +107,8 @@ export const MapConatiner = () => {
               width="60"
               className="pr-8 "
             ></motion.img>
-            <div className="cursor-pointer">
-              <div
-                className=" text-white font-desc text-descnew"
-                ref={SureyRef}
-              >
-                Surrey
-              </div>
+            <div>
+              <div className=" text-white font-desc text-descnew">Surrey</div>
             </div>
           </div>
           <div className="flex flex-row items-center content-start mt-2">
@@ -140,13 +121,8 @@ export const MapConatiner = () => {
               width="60"
               className="pr-8 "
             ></motion.img>
-            <div className="cursor-pointer">
-              <div
-                className=" text-white font-desc text-descnew"
-                ref={EdmentonRef}
-              >
-                Edmonton
-              </div>
+            <div>
+              <div className=" text-white font-desc text-descnew">Edmonton</div>
             </div>
           </div>
         </div>
