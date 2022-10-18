c7128fc5c421ca625bc4637bfa763cb04b2f8721
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-10-18 23:23:56 +0530

    changes

--- src/pages/carrear/index.js
+++ src/pages/carrear/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-
+import { useEffect } from 'react';
 export const Career = () => {
   console.log('careers');
-
+  useEffect(() => {
+    (function () {
+      var r,
+        d = document,
+        gt = d.getElementById,
+        cr = d.createElement,
+        tg = d.getElementsByTagName,
+        id = 'aidaform-embed';
+      if (!gt.call(d, id)) {
+        r = cr.call(d, 'script');
+        r.id = id;
+        r.src = 'https://embed.aidaform.com/embed.js';
+        (d.head || tg.call(d, 'head')[0]).appendChild(r);
+      }
+    })();
+  }, []);
   return (
     <div className="mt-28 ">
       {/* <div>Form</div> */}
