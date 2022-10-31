a3bbc66a4bff799964a481aae2104810704f61e8
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-10-31 20:30:26 +0530

    changes in nodeserver

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -25,112 +25,112 @@ app.use('/api/subscribe', subscribe);
 app.get('/', (req, res) => {
   res.send('Welcome to my api');
 });
-const multer = Multer({
-  storage: Multer.memoryStorage(),
-  limits: {
-    fileSize: 25 * 1024 * 1024, // no larger than 5mb, you can change as needed.
-  },
-});
-const googleStorage = new Storage({
-  keyFilename: path.join(__dirname, './kal-partz-d9e09365fa43.json'),
-  projectId: 'kal-partz',
-}); // add the bucket name - the bolded bit is what is supposed to be
+// const multer = Multer({
+//   storage: Multer.memoryStorage(),
+//   limits: {
+//     fileSize: 25 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+//   },
+// });
+// const googleStorage = new Storage({
+//   keyFilename: path.join(__dirname, './kal-partz-d9e09365fa43.json'),
+//   projectId: 'kal-partz',
+// }); // add the bucket name - the bolded bit is what is supposed to be
 
-googleStorage.getBuckets().then((x) => console.log(x));
-const bucket = googleStorage.bucket('kalgroup_files');
-app.post('/load', multer.single('file'), (req, res) => {
-  var RealPath = '';
+// googleStorage.getBuckets().then((x) => console.log(x));
+// const bucket = googleStorage.bucket('kalgroup_files');
+// app.post('/load', multer.single('file'), (req, res) => {
+//   var RealPath = '';
 
-  var data = req.body;
+//   var data = req.body;
 
-  try {
-    if (!data.file) {
-      return res.status(400).send({ message: `Please upload a file! ${data}` });
-    }
+//   try {
+//     if (!data.file) {
+//       return res.status(400).send({ message: `Please upload a file! ${data}` });
+//     }
 
-    const blob = bucket.file(data.file.originalname);
-    const blobStream = blob.createWriteStream({
-      resumable: false,
-    });
+//     const blob = bucket.file(data.file.originalname);
+//     const blobStream = blob.createWriteStream({
+//       resumable: false,
+//     });
 
-    blobStream.on('error', (err) => {
-      res.status(500).send({ message: err.message });
-    });
+//     blobStream.on('error', (err) => {
+//       res.status(500).send({ message: err.message });
+//     });
 
-    // console.log(`data is ${data.name}`);
+//     // console.log(`data is ${data.name}`);
 
-    blobStream.on('finish', async (data) => {
-      // create a url to access file
+//     blobStream.on('finish', async (data) => {
+//       // create a url to access file
 
-      const publicURL = format({
-        path: `https://storage.googleapis.com/${bucket.name}/${blob.name}`,
-      });
-      RealPath = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-      try {
-        await bucket.file(data.file.originalname).makePublic();
-      } catch {
-        return res.status(500).send({
-          message: `Uploaded the file successfully: ${data.file.originalname}`,
-          url: publicURL.path,
-        });
-      }
+//       const publicURL = format({
+//         path: `https://storage.googleapis.com/${bucket.name}/${blob.name}`,
+//       });
+//       RealPath = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+//       try {
+//         await bucket.file(data.file.originalname).makePublic();
+//       } catch {
+//         return res.status(500).send({
+//           message: `Uploaded the file successfully: ${data.file.originalname}`,
+//           url: publicURL.path,
+//         });
+//       }
 
-      console.log(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
-      res.status(200).send({
-        message: 'Uploaded the file successfully: ' + data.file.originalname,
-        url: `https://storage.googleapis.com/${bucket.name}/${blob.name}`,
-      });
-    });
+//       console.log(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+//       res.status(200).send({
+//         message: 'Uploaded the file successfully: ' + data.file.originalname,
+//         url: `https://storage.googleapis.com/${bucket.name}/${blob.name}`,
+//       });
+//     });
 
-    blobStream.end(data.file.buffer);
-  } catch (err) {
-    if (err.code === 'LIMIT_FILE_SIZE') {
-      return res.status(500).send({
-        message: 'File size cannot be larger than 25MB!',
-      });
-    }
+//     blobStream.end(data.file.buffer);
+//   } catch (err) {
+//     if (err.code === 'LIMIT_FILE_SIZE') {
+//       return res.status(500).send({
+//         message: 'File size cannot be larger than 25MB!',
+//       });
+//     }
 
-    res.status(500).send({
-      message: `Could not upload the file: ${data.file.originalname}. ${err}`,
-    });
-  }
-  var mailOptions = {
-    from: data.email,
-    to: () => {
-      if (data.site.includes('@')) {
-        return data.site;
-      }
-      if (data.site === 'kalgroup') {
-        return 'info@kalgroup.com';
-      } else {
-        return 'info@bigrigcanada.com';
-      }
-    },
+//     res.status(500).send({
+//       message: `Could not upload the file: ${data.file.originalname}. ${err}`,
+//     });
+//   }
+//   var mailOptions = {
+//     from: data.email,
+//     to: () => {
+//       if (data.site.includes('@')) {
+//         return data.site;
+//       }
+//       if (data.site === 'kalgroup') {
+//         return 'info@kalgroup.com';
+//       } else {
+//         return 'info@bigrigcanada.com';
+//       }
+//     },
 
-    bcc: ['pkumar@kalfreight.com', 'ssoam@kalfreight.com'],
-    subject: 'Contact Form Submission"',
-    html: `<p>Name:${data.name}</p>
-              <p>Email:${data.email}</p>
-              <p>Phone no:${data.phoneno}</p>
-              <p>message:${data.message}</p>
-              <p>location:${data.location}</p>
-              `,
-    attachments: [
-      {
-        path: RealPath,
-      },
-    ],
-  };
+//     bcc: ['pkumar@kalfreight.com', 'ssoam@kalfreight.com'],
+//     subject: 'Contact Form Submission"',
+//     html: `<p>Name:${data.name}</p>
+//               <p>Email:${data.email}</p>
+//               <p>Phone no:${data.phoneno}</p>
+//               <p>message:${data.message}</p>
+//               <p>location:${data.location}</p>
+//               `,
+//     attachments: [
+//       {
+//         path: RealPath,
+//       },
+//     ],
+//   };
 
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send('Success');
-    }
-    smtpTransport.close();
-  });
-});
+//   smtpTransport.sendMail(mailOptions, (error, response) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.send('Success');
+//     }
+//     smtpTransport.close();
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`app Started on ${port}`);
